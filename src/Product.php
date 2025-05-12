5ad90a785744545ee4e3b2e2f9695799636fc5f4
oger-gabriel <168653684+oger-gabriel@users.noreply.github.com>
2025-05-12 14:14:32 +0200

    Commençons par l'entité produit (partie 2)

--- src/Product.php
+++ src/Product.php
@@ -12,4 +12,24 @@ class Product
     private int|null $id = null;
     #[ORM\Column(type: "string")]
     private string $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
 }
